092743a5f63bba2b548c1ef5a1628f1812eb2def
Cenafowzin <digodubeux@gmail.com>
2024-09-29 13:13:31 -0300

    corrigindo cartao

--- coolmeia/administracao/src/main/java/administracao/cliente/Cartao.java
+++ coolmeia/administracao/src/main/java/administracao/cliente/Cartao.java
@@ -2,23 +2,28 @@ package administracao.cliente;
 
 import org.jmolecules.ddd.types.ValueObject;
 import static org.apache.commons.lang3.Validate.notNull;
+import static org.apache.commons.lang3.Validate.notBlank;
 import java.util.Objects;
 import static org.apache.commons.lang3.Validate.isTrue;
 
 public class Cartao implements ValueObject {
+    private final String nome;
     private final String numero;
     private final String validade;
     private final String cvv;
 
-    public Cartao(String numero, String validade, String cvv) {
+    public Cartao(String nome, String numero, String validade, String cvv) {
+        notNull(nome, "O nome do titular não pode ser nulo");
+        notBlank(nome, "O nome do titular não pode ser vazio");
         notNull(numero, "O número do cartão não pode ser nulo");
         notNull(validade, "A validade não pode ser nula");
         notNull(cvv, "O CVV não pode ser nulo");
-        
+
         isTrue(validarNumero(numero), "O número do cartão de crédito é inválido");
         isTrue(validarValidade(validade), "A validade do cartão é inválida");
         isTrue(validarCvv(cvv), "O CVV do cartão é inválido");
 
+        this.nome = nome;
         this.numero = numero;
         this.validade = validade;
         this.cvv = cvv;
@@ -36,6 +41,10 @@ public class Cartao implements ValueObject {
         return cvv.matches("\\d{3}");
     }
 
+    public String getNome() {
+        return nome;
+    }
+
     public String getNumero() {
         return numero;
     }
@@ -51,8 +60,9 @@ public class Cartao implements ValueObject {
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof Cartao) {
-        	Cartao outro = (Cartao) obj;
-            return numero.equals(outro.numero) &&
+            Cartao outro = (Cartao) obj;
+            return nome.equals(outro.nome) &&
+                   numero.equals(outro.numero) &&
                    validade.equals(outro.validade) &&
                    cvv.equals(outro.cvv);
         }
@@ -61,11 +71,11 @@ public class Cartao implements ValueObject {
 
     @Override
     public int hashCode() {
-        return Objects.hash(numero, validade, cvv);
+        return Objects.hash(nome, numero, validade, cvv);
     }
 
     @Override
     public String toString() {
-        return String.format("Cartão de Crédito: %s, Válido até: %s", numero, validade);
+        return String.format("Cartão de Crédito de %s: %s, Válido até: %s", nome, numero, validade);
     }
 }
